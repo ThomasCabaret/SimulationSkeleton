7d7ee9b251d3d7d649280cb3a1e08f89f637fc88
ThomasCabaret <thomas.cabaret@gmail.com>
2023-05-22 13:26:59 +0200

    Polar particles

--- main.cpp
+++ main.cpp
@@ -2,22 +2,45 @@
 #include <vector>
 #include <thread>
 #include <random>
+#include <cmath>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 // Simple struct to hold particle data
 struct Particle {
     sf::Vector2f position;
     sf::Vector2f velocity;
+    float orientation;
+    float angularVelocity;
+    sf::Vector2f force;
+    float torque;
     int type;
-    std::vector<Particle*> linked;
+    //std::vector<Particle*> linked;
     //Having view relating object in the model object is not ideal
     //but in SFML not rebuilding the graphical object is significantly faster
     sf::CircleShape shape;
 };
 
+sf::Vector2f multiply(const sf::Vector2f& vec, float scalar) {
+    return sf::Vector2f(vec.x * scalar, vec.y * scalar);
+}
+
 static int WORLD_WIDTH = 1920;
 static int WORLD_HEIGTH = 1080;
 static int DOT_SIZE = 2;
 static sf::Vector2f DOT_OFSET = sf::Vector2f(DOT_SIZE, DOT_SIZE);
+
+float norm(const sf::Vector2f& vec) {
+    return std::sqrt(vec.x * vec.x + vec.y * vec.y);
+}
+
+void normalize(sf::Vector2f& vec)
+{
+    float normVec = norm(vec);
+    if (!normVec == 0) vec /= normVec;
+}
 
 sf::Color getColor(int type) {
     // Convert the type to a hue value between 0 and 360 degrees
@@ -52,8 +75,10 @@ sf::Color getColor(int type) {
 
 struct Model {
     std::vector<Particle> particles;
+    std::random_device rd;
+    std::mt19937 gen;
 
-    Model()
+    Model() : gen(rd())
     {
         init();
     }
@@ -61,12 +86,11 @@ struct Model {
     void init()
     {
         // Initialize random number generator
-        std::random_device rd;
-        std::mt19937 gen(rd());
         std::uniform_int_distribution<> disType(0, 15);
-        std::uniform_real_distribution<> disX(-WORLD_WIDTH/2, WORLD_WIDTH/2);
-        std::uniform_real_distribution<> disY(-WORLD_HEIGTH/2, WORLD_HEIGTH/2);
+        std::uniform_real_distribution<> disX(-WORLD_WIDTH/4, WORLD_WIDTH/4);
+        std::uniform_real_distribution<> disY(-WORLD_HEIGTH/4, WORLD_HEIGTH/4);
         std::uniform_real_distribution<> disV(-2, 2);
+        std::uniform_real_distribution<> disA(0, 2.0*M_PI);
 
         // Initialize particles
         particles.clear();
@@ -74,43 +98,156 @@ struct Model {
             Particle p;
             p.position = sf::Vector2f(disX(gen), disY(gen));
             p.velocity = sf::Vector2f(disV(gen), disV(gen));
+            p.orientation = disA(gen);
             p.type = disType(gen);
             p.shape = sf::CircleShape(DOT_SIZE);
+            p.shape.setOrigin(DOT_SIZE, DOT_SIZE);
             p.shape.setPosition(p.position);
             particles.push_back(p);
         }
     }
 
+    void calculateForceAndTorque(const sf::Vector2f& r, float rNorm, float orientation1, float orientation2, sf::Vector2f& force, float& torque)
+    {
+        // The interaction strength
+        float strengthF = 100.0f;
+        float strengthT = 1.0f;
+
+        // Calculate the relative orientation
+        float deltaOrientation = orientation2 - orientation1;
+
+        // Make sure deltaOrientation is between -pi and pi
+        while (deltaOrientation > M_PI) deltaOrientation -= 2 * M_PI;
+        while (deltaOrientation < -M_PI) deltaOrientation += 2 * M_PI;
+
+        // Calculate the force
+        // The force magnitude depends on the relative orientation
+        float forceMagnitude = strengthF * deltaOrientation;
+
+        // The force direction is perpendicular to the relative position
+        force = sf::Vector2f(-r.y, r.x) * (forceMagnitude / rNorm);
+
+        // Calculate the torque
+        // The torque magnitude depends on the relative orientation and the distance
+        torque = strengthT * deltaOrientation * rNorm;
+    }
+
     void step()
     {
-        const float attractionStrength = 0.05f;
-        const float linkingThreshold = 30.0f;
+        //const float attractionStrength = 0.05f;
+        const float interactionRadius = 30.0f;
+        //const float attractionMinThreshold = 5.0f;
+        //const float linkingThreshold = 30.0f;
 
+        // Calculate the force and torque on particle p due to all other particles
         for (auto& p : particles) {
-            // Update particle position
-            p.position += p.velocity;
-
-            // Look for bound creation
-            for (auto& other : particles) {
-                if (&p == &other) continue;
-
-                sf::Vector2f delta = other.position - p.position;
-                float distance = std::sqrt(delta.x*delta.x + delta.y*delta.y);
-
-                // If within linking threshold, link the particles
-                if (distance < linkingThreshold && p.linked.size() < 2 && other.linked.size() < 2) {
-                    p.linked.push_back(&other);
-                    other.linked.push_back(&p);
+            for (Particle& other : particles) {
+                if (&other != &p) {  // Avoid self-interaction
+                    sf::Vector2f r = other.position - p.position;
+                    float rNorm = norm(r);
+                    if (rNorm < interactionRadius) {  // Consider only particles within the interaction radius
+                        // Calculate force and torque using appropriate model
+                        sf::Vector2f force;
+                        float torque;
+                        calculateForceAndTorque(r, rNorm, p.orientation, other.orientation, force, torque);
+                        p.force += force;
+                        p.torque += torque;
+                    }
                 }
             }
 
-            // Apply force
-            for (auto& other : p.linked) {
-                sf::Vector2f delta = other->position - p.position;
-                float distance = std::sqrt(delta.x*delta.x + delta.y*delta.y);
-                delta /= distance; // Normalize delta
-                p.velocity += delta * attractionStrength;
+            // Containing forces
+            if (p.position.x > WORLD_WIDTH/2) p.force += sf::Vector2f(-0.1,0.0);
+            if (p.position.x < -WORLD_WIDTH/2) p.force += sf::Vector2f(0.1,0.0);
+            if (p.position.y > WORLD_HEIGTH/2) p.force += sf::Vector2f(0.0,-0.1);
+            if (p.position.y < -WORLD_HEIGTH/2) p.force += sf::Vector2f(0.0,0.1);
+        }
+
+        for (auto& p : particles) {
+            // Calculate the acceleration and angular acceleration (assuming mass and moment of inertia = 1)
+            sf::Vector2f acceleration = p.force;
+            float angularAcceleration = p.torque;
+            float dt = 0.01;
+
+            // Using naive algo
+            p.velocity = p.velocity + acceleration * dt;
+            p.angularVelocity = p.angularVelocity + angularAcceleration * dt;
+            float maxVelocity = 50.0;
+            if (norm(p.velocity) > maxVelocity) {
+                normalize(p.velocity);
+                p.velocity *= maxVelocity;
             }
+            float maxAngularVelocity = 10.0;
+            if (p.angularVelocity > maxAngularVelocity) p.angularVelocity = maxAngularVelocity;
+            if (p.angularVelocity < maxAngularVelocity) p.angularVelocity = -maxAngularVelocity;
+            p.position = p.position + p.velocity * dt;
+            p.orientation = p.orientation + p.angularVelocity * dt;
+
+            // Update position and velocity using Velocity Verlet algorithm
+            //p.position = p.position + p.velocity * dt + 0.5f * acceleration * dt * dt;
+            //sf::Vector2f newAcceleration = p.force;  // Recalculate force here if necessary
+            //p.velocity = p.velocity + 0.5f * (acceleration + newAcceleration) * dt;
+
+            // Update orientation and angular velocity
+            //p.orientation = p.orientation + p.angularVelocity * dt + 0.5f * angularAcceleration * dt * dt;
+            //float newAngularAcceleration = p.torque;  // Recalculate torque here if necessary
+            //p.angularVelocity = p.angularVelocity + 0.5f * (angularAcceleration + newAngularAcceleration) * dt;
+
+            // Update the particle's shape position
+            p.shape.setPosition(p.position);
+            p.shape.setFillColor(getColor(p.type));
+        }
+
+            // sort by distance
+            //std::sort(neighbors.begin(), neighbors.end());
+
+            //int nCount = 0;
+            //int dCut = 8;
+            //for (auto& pair : neighbors) {
+            //    nCount++;
+            //    float distance = pair.first;
+            //    Particle* other = pair.second;
+
+            //    sf::Vector2f delta = other->position - p.position;
+            //    delta /= distance; // Normalize delta
+            //    float f = 1.0*(dCut-(nCount-1.0))/dCut;
+            //    if (f < -0.2) f = -0.2;
+            //    if (distance > attractionMinThreshold) {
+            //        p.velocity += f * delta * attractionStrength;
+            //    } else {
+            //        float rep = 0.2;
+            //        p.velocity -= rep * delta * attractionStrength;
+            //    }
+                //if (nCount >= dCut){
+                //    p.velocity -= delta * attractionStrength;
+                //}
+
+                // Floc behavior
+            //    p.velocity = multiply(p.velocity, 0.999);
+            //    p.velocity += multiply(other->velocity, 0.0001);
+            //}
+
+            //std::uniform_real_distribution<> disPV(-0.1, 0.1);
+            //p.velocity += sf::Vector2f(disPV(gen),disPV(gen));
+
+            //p.velocity -= multiply(p.position, 0.001);
+
+            //if (norm(p.velocity) < 1.0) {
+            //    normalize(p.velocity);
+            //}
+
+            //if (norm(p.velocity) > 2.0) {
+            //    p.velocity = multiply(p.velocity, 0.9);
+            //}
+
+            // Apply force
+            //for (auto& other : p.linked) {
+            //    sf::Vector2f delta = other->position - p.position;
+            //    float distance = std::sqrt(delta.x*delta.x + delta.y*delta.y);
+            //    delta /= distance; // Normalize delta
+            //    p.velocity += delta * attractionStrength;
+            //}
+            //normalize(p.velocity);
 
             // TODO Torus world is problematic to draw lines between points
             // Torus world
@@ -118,29 +255,31 @@ struct Model {
             //if (p.position.x < -WORLD_WIDTH/2) p.position.x += WORLD_WIDTH;
             //if (p.position.y > WORLD_HEIGTH/2) p.position.y -= WORLD_HEIGTH;
             //if (p.position.y < -WORLD_HEIGTH/2) p.position.y += WORLD_HEIGTH;
-
-            // Update the particle's shape position
-            p.shape.setPosition(p.position);
-            p.shape.setFillColor(getColor(p.type));
-        }
     }
 };
 
 void drawModel(sf::RenderWindow& ioWindow, const Model& iModel) {
     for (const auto& p : iModel.particles)
     {
-        for (auto& other : p.linked) {
+        /*for (auto& other : p.linked) {
             sf::VertexArray lines(sf::Lines, 2);
             lines[0].position = p.position + DOT_OFSET;
             lines[1].position = other->position + DOT_OFSET;
             lines[0].color = sf::Color::White;
             lines[1].color = sf::Color::White;
             ioWindow.draw(lines);
-        }
+        }*/
 
+        float tailLength = 5.0;
+        sf::Vertex line[] =
+        {
+            sf::Vertex(p.position, sf::Color::White),
+            sf::Vertex(p.position + tailLength*sf::Vector2f(std::cos(p.orientation), std::sin(p.orientation)), sf::Color::White)
+        };
+
+        ioWindow.draw(line, 2, sf::Lines);
         ioWindow.draw(p.shape);
     }
-
 }
 
 // Main function
